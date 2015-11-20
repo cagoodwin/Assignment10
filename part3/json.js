609834b81c2b8f26583f9e16342db2f08a565fce
unknown <careyanne@gmail.comgitgit clear>
2015-11-19 19:08:43 -0800

    completed

---
+++ part3/json.js
@@ -0,0 +1,48 @@
+{"dogs":[
+	
+{
+    "name":"Thule",
+
+
+    "breed":"Australian Cattle Dog",
+
+    "likes":"Playing fetch, going for hikes, sleeping on the couch."
+},
+    
+{
+    "name":"Stewart",
+
+
+    "breed":"Beagle Mix",
+
+    "likes":"Going for walks, barking, sleeping on the couch."
+},
+
+{
+    "name":"Sage",
+
+
+    "breed":"Black Lab",
+
+    "likes":"Playing fetch, going for hikes, swimming."
+},
+    
+{
+    "name":"Huri",
+
+
+    "breed":"Border Collie",
+
+    "likes":"Playing ball, gswimming, sleeping on the couch."
+},
+    
+{
+    "name":"Lola",
+
+
+    "breed":"Shepherd Mix",
+
+    "likes":"Chewing on bones, playing with dogs, sleeping on the couch."
+}
+
+]}
